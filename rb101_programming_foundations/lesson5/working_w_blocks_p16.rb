c5e60ffe746a410cc51bd47f01764dee17904a2a
Michael B Lawrence <007mike@gmail.com>
2020-10-10 13:46:14 -0400

    ending lesson5

--- rb101_programming_foundations/lesson5/working_w_blocks_p16.rb
+++ rb101_programming_foundations/lesson5/working_w_blocks_p16.rb
@@ -4,11 +4,10 @@ CHOICES = %w(a b c d e f 0 1 2 3 4 5 6 7 8 9)
 def uuid
   uuid_str = ''
   UUID_FORMAT.each_with_index do |int, idx|
-    int.times do |x|
-      uuid_str << CHOICES.sample
-    end
-  uuid_str << '-' unless idx >= 3
+    int.times  {|x| uuid_str << CHOICES.sample }
+    uuid_str << '-' unless idx >= 3
   end
+  
   uuid_str
 end
 
